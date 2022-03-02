28df1540a3844221166e32e4fd4cef6432b7b1ab
Mary296 <guskowa.masha@yandex.ru>
2022-03-02 19:23:10 +0700

    Sort packages

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import argparse
 import collections
 import datetime
-
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 import pandas
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 WINERY_OPENING_YEAR = 1920
@@ -17,11 +15,13 @@ if __name__ == '__main__':
     parser.add_argument('--file_path', default='products.xlsx')
     args = parser.parse_args()
 
-    wines = pandas.read_excel(args.file_path, sheet_name='Лист1', na_values=['N/A', 'NA'], keep_default_na=False)\
+    wines = pandas.read_excel(args.file_path,
+                              sheet_name='Лист1',
+                              na_values=['N/A', 'NA'],
+                              keep_default_na=False)\
         .to_dict(orient='records')
 
     grouped_products = collections.defaultdict(list)
-
     for wine in wines:
         grouped_products[wine['Категория']].append(wine)
 
@@ -31,7 +31,6 @@ if __name__ == '__main__':
     )
 
     template = env.get_template('template.html')
-
     rendered_page = template.render(
         winery_age=datetime.datetime.now().year - WINERY_OPENING_YEAR,
         products=grouped_products)
